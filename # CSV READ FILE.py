70b0b4f074b1c082aa3078f479436d4a6a32c3e0
talha195 <47970963+talha195@users.noreply.github.com>
2022-05-12 01:19:47 +0500

    Add files via upload

---
+++ # CSV READ FILE.py
@@ -0,0 +1,14 @@
+# CSV READ FILE
+
+csv_file = pd.read_csv('File_path', index_col=False)
+for key,value in csv_file.iterrows():
+    account = value['Accounts']
+    accounts = account.split(':')
+    driver = webdriver.Chrome('chromedriver_path')
+    driver.get("https://www.reddit.com/")
+    driver.find_element_by_xpath("//div//fieldset//input[@id='loginUsername']").send_keys(accounts[0])
+    sleep(5)
+    driver.find_element_by_xpath("//div//fieldset//input[@id='loginPassword']").send_keys(accounts[1])
+    sleep(5)
+    driver.find_element_by_xpath("//div//button[contains(text(),'Log In')]").click()
+    sleep(2)
